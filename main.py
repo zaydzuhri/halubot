55c173e21295600af126f79d258cb7da6fa75ed1
zaydzuhri <zaydzuhri@gmail.com>
2021-06-19 22:58:29 +0700

    added nyan command lmao

--- main.py
+++ main.py
@@ -19,11 +19,18 @@ async def on_message(message):
         return
 
     if msg.startswith('>'):
-        command = msg[1:]
+        command = msg[1:].lower()
         if command == "hello":
-            await message.channel.send("Hello World!!")
+            await message.channel.send("Hello World!")
         elif command == "random":
             number = random.randrange(100000)
             await message.channel.send(f"Here's your random number: {number}")
-            
+        elif command == "nyan":
+            nyan_len = random.randrange(1, 10)
+            nyan = "nya"
+            for i in range(nyan_len):
+                nyan += "a"
+            nyan += "n!"
+            await message.channel.send(f"{nyan}")
+
 client.run(DISCORD_TOKEN)
